7a3502f7c9174494054c029d9889f8179c31adfb
nnutica <nitinat.loru@bumail.net>
2024-11-30 15:51:07 +0700

    r

--- src/component/navbar/Navbar.tsx
+++ src/component/navbar/Navbar.tsx
@@ -25,7 +25,7 @@ function Navbar() {
                 {/* Login Button */}
                 <div className="hidden md:flex items-center space-x-4">
                     <a
-                        href="#login"
+                        href="/login"
                         className="text-blue-600 border border-blue-600 px-6 py-2 rounded-lg hover:bg-blue-600 hover:text-white"
                     >
                         Login
