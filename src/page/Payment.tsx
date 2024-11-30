4a3b396f92930a09528d3681768d3340f63d3f62
nnutica <nitinat.loru@bumail.net>
2024-11-30 13:17:14 +0700

    pay

--- src/page/Payment.tsx
+++ src/page/Payment.tsx
@@ -11,6 +11,8 @@ const PaymentPage = () => {
         /* Modal Component */
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
             <h1 className="text-3xl font-bold mb-6">Payment</h1>
+            {/* แก้ตรงนี้ เป็นคล้ายๆหน้า สรุปค่าใช้จ่ายที่ต้องชำระ*/}
+
             <button
                 onClick={handleOpenModal}
                 className="bg-blue-500 text-white px-6 py-3 rounded-lg shadow"
